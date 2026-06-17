b2658e62e0c290fffe5df0bfcfedd8fc5cdff6ac
SCLim999 <seng-chee.lim@vitrox.edu.my>
2026-06-18 07:53:31 +0800

    include scanner to read data

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner sc = new Scanner(System.in);
+
         System.out.printf("Hello and welcome!");
 
 
